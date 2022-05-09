6ee5b972f5f082f2499958a72b6a1501d1a5b170
lithika-damnod <lithika-damnod@Lithikas-MacBook-Air.local>
2022-05-09 11:11:05 +0530

    created some sql files for configuring a table that teacher had given in a revision paper for sql practicing

---
+++ practice-sql/push_customerdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customer VALUES ("C1", "Mr  Dave", "Kandy", "848402820");
+INSERT INTO Customer VALUES ("C2", "Mrs Smith", "Galle", "819749484");
+INSERT INTO Customer VALUES ("C3", "Mr James", "Colombo", "591828484");
+INSERT INTO Customer VALUES ("C4", "Mr  Brown", "Kandy", "848406633");
+INSERT INTO Customer VALUES ("C5", "MrS White", "Ratnapura", "848499282");
---
+++ practice-sql/push_orderdata.sql
@@ -0,0 +1,35 @@
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R011", "C1", "P0127625", 3); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R011", "C1", "P0457383", 1); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R025", "C4", "P0636239", 1); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R028", "C5", "P0127635", 4); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R028", "C5", "P0338786", 1); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R028", "C5", "P0536838", 2); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R033", "C2", "P0127635", 2); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R033", "C2", "P0536838", 1); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R036", "C1", "P0281637", 3); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R038", "C3", "P0338786", 2); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R038", "C3", "P0457383", 1); 
+
+INSERT INTO Orders (OrderNo, Cust_Id, P_No, Quantity) 
+VALUES("R038", "C3", "P0287627", 2); 
